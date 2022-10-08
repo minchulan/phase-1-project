24f25298d18daaed2829d35d1d4d979042d77b25
Minchul An <min@minchulan.io>
2022-10-08 01:06:21 -0400

    updated code in index.js, specifically makeShows and getShows functions.

--- index.js
+++ index.js
@@ -1,107 +1,53 @@
-// get data using fetch
-    // set up a function that gets the show data and then o something with the converted data
+document.addEventListener("DOMContentLoaded", getShows);
+
 function getShows() {
-    fetch('https://api.tvmaze.com/shows')
-    .then((res) => res.json())
-    // .then(shows => console.log("shows", shows))   
-    .then(shows => shows.forEach(show => renderShows(show)))
-    .catch(error => alert(error))  
+    const form = document.querySelector("#show-form");
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      const searchTerm = document.querySelector("#show-input").value;
+      fetch(`https://api.tvmaze.com/search/shows?q=${searchTerm}`)
+      .then(res => res.json())
+      .then(data => data.length > 0 ? data.forEach(makeShows) : returnNone())
+      .catch(error => console.log(`Error: ${error}`))
+      e.target.reset()
+      container.innerHTML = ""
+    })
 }
 
-getShows();
+const container = document.querySelector(".show-section");
 
-
-// set up a function that generates each card by rendering data to the DOM
-    // note: function declaration renderShow() will be hoisted.
-function renderShows(show) {
-    const showCollection = document.getElementById("show-list"); // div section will contain all search results
+function makeShows(show) {
     const div = document.createElement("div")
-    const h2 = document.createElement("h2");
-    h2.textContent = "📺";
     const img = document.createElement("img")
-    img.src = show.image.medium
-    img.alt = "show cover image"
-    img.classList.add("show-image");
-    const h3 = document.createElement("h3");
-    h3.textContent = show.name;
-    // const h4 = document.createElement("h4")
-    // h4.textContent = `Rating: ${show.rating.average}`
-    const p = document.createElement("p");
-    p.textContent = `
-        Rating: ${show.rating.average}
-        Genre: ${show.genres}
-        Summary: ${show.summary}
-        Link: ${show.url}
-    `
-    const button = document.createElement("button")
-    button.classList.add("watch-btn")
-    button.textContent = "Watch"
-    button.addEventListener("click", () => window.open(show.url))
+    const name = document.createElement("name")
+    const rating = document.createElement('rating')
+    const button = document.createElement('button')
 
-    showCollection.appendChild(div)   
-    div.append(h2, h3, p, button, img);
+    div.setAttribute("class", "show-card");
+    img.setAttribute('class', 'show-image')
+    name.setAttribute('class', 'show-name')
+    rating.setAttribute('class', 'show-rating')
+    button.setAttribute('class', 'watch-now')
+   
+    name.innerText = show.show.name
+    img.src = show.show.image.medium
+    img.alt = 'show cover image'
+    rating.innerText = `Rating: ${show.show.rating.average}`
+    button.innerText = 'Watch Now'
+    button.addEventListener("click", () => window.open(show.show.url));
+    
+    div.append(name, img, rating, button);
+    container.append(div); 
 }
-
-// set up a function that generates a card if we don't get any data back.
+ 
 function returnNone() {
-    const div = document.createElement("div")
-    div.className = "show-class"
-    const h2 = document.createElement("h2")
-    h2.textContent = "☹️"
-    const h3 = document.createElement("h3")
-    h3.textContent = "No Shows Found"
+        const div = document.createElement("div");
+        div.setAttribute("class", "show-card");
+        const h2 = document.createElement("h2");
+        h2.textContent = "☹️";
+        const h3 = document.createElement("h3");
+        h3.textContent = "No Shows Found";
 
-    showCollection.appendChild(div);
-    div.append(h1, h3)
-
+        container.appendChild(div);
+        div.append(h2, h3);
 }
-
-
-
-const corsHeaders = {
-    'Access-Control-Allow-Headers' : '*',
-    'Access-Control-Allow-Methods': 'POST',
-    'Access-Control-Allow-Origin' : '*'
-}
-
-// Generate a card based on user input using the search API endpoint 
-const form = document.querySelector("#show-form");
-form.addEventListener("submit", function (e) {
-    e.preventDefault()
-    const searchTerm = e.target[0].value
-
-    async function handleRequest(request) {
-        if (request.method === "OPTIONS") {
-          return new Response("OK", { headers: corsHeaders });
-        }
-        if (request.method === "POST") {
-          return getImages(request);
-        }
-    }
-
-        const getImages = async request => {
-            const { query } = await request.json()
-            fetch(`https://api.tvmaze.com/search/shows/${searchTerm}`)
-            .then((res) => res.json())
-            .then((data) => {
-                const img = document.createElement("img");
-                createImages(show);
-                // title, summary, image, rating, genre, link
-        
-            })  
-    }
-});
-
-
-const createImages = (shows) => {
-    for (let show of shows) {
-        if (show.image) {
-            const img = document.createElement("img")
-             img.src = show.image.medium;
-             img.alt = "show cover image";
-             document.body.append(img);
-        } 
-    }
-}
-
-
