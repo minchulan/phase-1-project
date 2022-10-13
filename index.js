ef1459533b1401d02f775e6522a62e9ebc5a6c9f
Minchul An <min@minchulan.io>
2022-10-13 02:28:01 -0400

    updated script file

--- index.js
+++ index.js
@@ -1,65 +1,71 @@
-document.addEventListener("DOMContentLoaded", () => {
-  getShows()
-})
+const handleLoadPage = () => {
+  getShows();
+};
+
+document.addEventListener("DOMContentLoaded", handleLoadPage);
+
 
 function getShows() {
-  const form = document.querySelector("#show-form")
+  const form = document.querySelector("#show-form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
-    const searchTerm = document.querySelector("#show-input").value
+    const searchTerm = document.querySelector("#show-input").value;
     fetch(`https://api.tvmaze.com/search/shows?q=${searchTerm}`)
-    .then((res) => res.json())
-    .then((shows) =>
-      shows.length > 0 ? shows.forEach(makeShows) : returnNone()
-    )
-    .catch((error) =>
-      console.log(`Error: ${error}`)
-    )
-    e.target.reset()
-    container.innerHTML = ""
-  })
+      .then((res) => res.json())
+      .then((shows) =>
+        shows.length > 0 ? shows.forEach(makeShows) : returnNone()
+      )
+      .catch(error => console.log(error));
+    e.target.reset();
+    container.innerHTML = "";
+  });
 }
 
-const container = document.querySelector(".show-section")
+const container = document.querySelector(".show-section");
 
 function makeShows(show) {
-  const card = document.createElement("div");
+  const div = document.createElement("div");
+  div.id = `show-card-${show.id}`;
+  div.className = "show-card";
+
   const img = document.createElement("img");
-  const name = document.createElement("h3");
-  const rating = document.createElement("h4");
-  const paragraph = document.createElement("p");
-  const button = document.createElement("button");
-
-  card.setAttribute("class", "show-card");
   img.setAttribute("class", "show-image");
-  name.setAttribute("class", "show-name");
-  rating.setAttribute("class", "show-rating");
-  paragraph.setAttribute("class", "show-paragraph");
-  button.setAttribute("class", "show-info");
-
-  name.innerText = show.show.name.toUpperCase();
   img.src = show.show.image.medium;
+
+  const h3 = document.createElement("h3");
+  h3.setAttribute("class", "show-name");
+  h3.innerText = show.show.name.toUpperCase();
+
+  const rating = document.createElement("h4");
+  rating.setAttribute("class", "show-rating");
   rating.innerText = `Rating: ${show.show.rating.average}`;
+
+  const paragraph = document.createElement("p");
+  paragraph.setAttribute("class", "show-paragraph");
   paragraph.innerText = show.show.summary.slice(3, -4);
-  button.innerText = "Show Link";
+
+  const button = document.createElement("button");
+  button.id = `show-button-${show.id}`
+  button.className = "show-link";
+  button.textContent = "Get Link";
   button.addEventListener("click", () => alert(show.show.url));
 
-  card.append(name, img, rating, paragraph, button);
-  container.append(card);
+  div.append(h3, img, rating, paragraph, button);
+  container.append(div);
 }
 
 function returnNone() {
-  const div = document.createElement("div")
-  div.setAttribute("class", "show-card")
-  const h3 = document.createElement("h3")
-  h3.textContent = "☹️ No Shows Found!"
-  container.appendChild(div)
-  div.append(h3)
+  const div = document.createElement("div");
+  div.setAttribute("class", "show-card");
+  const h3 = document.createElement("h3");
+  h3.textContent = "☹️ No Shows Found!";
+  container.appendChild(div);
+  div.append(h3);
 }
 
-const input = document.querySelector('#show-input')
-const h2 = document.querySelector('h2')
-input.addEventListener("input", e => h2.innerText = input.value);
+const input = document.querySelector("#show-input");
+const h2 = document.querySelector("h2");
+input.addEventListener("input", (e) => (h2.innerText = input.value));
 
-const logo = document.querySelector("#teleSearchLogo")
-logo.addEventListener("mouseover", e => console.log(e))
+const logo = document.querySelector("#teleSearchLogo");
+logo.addEventListener("mouseover", (e) => console.log(e));
