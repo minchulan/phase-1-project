5efaf7aa0ee0c3be07e319747423af0c9a61efc9
Minchul An <min@minchulan.io>
2022-10-14 00:17:32 -0400

    organized script file by groupings - constants, functions, addEventListeners.

--- index.js
+++ index.js
@@ -1,25 +1,37 @@
+// DOMContentLoaded
 window.addEventListener("DOMContentLoaded", () => {
   getShows()
 });
 
+// global constants
+const form = document.querySelector("#show-form");
+const container = document.querySelector(".show-section");
+const input = document.querySelector("#show-input");
+const h2 = document.querySelector("h2");
+const searchBar = document.querySelector("#show-input");
+const makeRandomColor = () => {
+  const r = Math.floor(Math.random() * 255);
+  const g = Math.floor(Math.random() * 255);
+  const b = Math.floor(Math.random() * 255);
+  return `rgb(${r}, ${g}, ${b})`;
+};
 
+// functions
 function getShows() {
-  const form = document.querySelector("#show-form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
     const searchTerm = document.querySelector("#show-input").value;
     fetch(`https://api.tvmaze.com/search/shows?q=${searchTerm}`)
-      .then((res) => res.json())
+    //.then(resp => console.log("res", resp))
+      .then((resp) => resp.json())
       .then((shows) =>
         shows.length > 0 ? shows.forEach(makeShows) : returnNone()
       )
-      .catch((error) => console.log(`This is my error: ${error}`))
+      .catch((error) => console.log(`This is my error: ${error}`));
     e.target.reset();
     container.innerHTML = "";
   });
 }
-
-const container = document.querySelector(".show-section");
 
 function makeShows(show) {
   const div = document.createElement("div");
@@ -27,7 +39,7 @@ function makeShows(show) {
   div.className = "show-card";
 
   const h3 = document.createElement("h3");
-  h3.setAttribute("class", "show-name");
+  h3.className = "show-name";
   h3.innerText = show.show.name.toUpperCase();
 
   const img = document.createElement("img");
@@ -35,15 +47,15 @@ function makeShows(show) {
   img.src = show.show.image.medium;
 
   const rating = document.createElement("h4");
-  rating.setAttribute("class", "show-rating");
+  rating.className = "show-rating";
   rating.innerText = `Rating: ${show.show.rating.average}`;
 
   const paragraph = document.createElement("p");
-  paragraph.setAttribute("class", "show-paragraph");
+  paragraph.className = "show-paragraph";
   paragraph.innerText = show.show.summary
     .slice(3, -4)
     .replace(/[^0-9a-zA-Z. ]/g, "");
-
+  
   const button = document.createElement("button");
   button.id = `show-button-${show.id}`;
   button.className = "show-link";
@@ -56,26 +68,18 @@ function makeShows(show) {
 
 function returnNone() {
   const div = document.createElement("div");
-  div.setAttribute("class", "show-card");
+  div.className = "show-card";
   const h3 = document.createElement("h3");
-  h3.textContent = "☹️ No Shows Found!";
+  h3.className = "show-name;"
+  h3.textContent = "No Shows Found 😕 ";
   container.appendChild(div);
   div.append(h3);
 }
 
-const input = document.querySelector("#show-input");
-const h2 = document.querySelector("h2");
+// event listeners
 input.addEventListener("input", (e) => (h2.innerText = input.value));
-
-const searchBar = document.querySelector("#show-input");
 searchBar.addEventListener(
   "keydown",
   (event) => (event.target.style.color = makeRandomColor())
 );
 
-function makeRandomColor() {
-  const r = Math.floor(Math.random() * 255);
-  const g = Math.floor(Math.random() * 255);
-  const b = Math.floor(Math.random() * 255);
-  return `rgb(${r}, ${g}, ${b})`;
-}
